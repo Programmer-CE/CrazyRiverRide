3c7623d12e948800e6a1e0886fac0d434f6a77d1
Ismael <ismael@ismael-Satellite-A215.(none)>
2015-02-19 23:52:23 -0600

    HPEntity aparece

---
+++ src/gui/hpentity.cpp
@@ -0,0 +1,19 @@
+#include "hpentity.h"
+
+HPEntity::HPEntity(int pMaxHp):_Hp(pHp),_MaxHp(pHp)
+{
+}
+
+void HPEntity::addHitPoints(int pHp){
+    if (pHp >= _Hp)_Hp = 0;
+    else if (pHp+_Hp > _MaxHp)_Hp = _MaxHp;
+    else _Hp += pHp;
+}
+
+bool HPEntity::isDead()const{
+    return _Hp == 0;
+}
+
+int HPEntity::getHitPoints()const{
+    return _Hp;
+}
---
+++ src/gui/hpentity.h
@@ -0,0 +1,17 @@
+#include "renderizable.h"
+#ifndef HPENTITY_H
+#define HPENTITY_H
+
+class HPEntity:public Renderizable
+{
+protected:
+    int _Hp;
+    int _MaxHp;
+public:
+    HPEntity(int pMaxHp);
+    bool isDead()const;
+    void addHitPoints(int pHp = -1);
+    int getHitPoints()const;
+};
+
+#endif // HPENTITY_H
