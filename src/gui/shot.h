85cd84f5fb2c6f6d17c1d38b2ef0c0eaf7a2638e
albinmora <albin.moravalverde@gmail.com>
2015-02-20 00:42:29 -0600

    Header LinearShot más modificacion del Header Shot

--- src/gui/shot.h
+++ src/gui/shot.h
@@ -1,10 +1,10 @@
 #include "hpentity.h"
 #ifndef SHOT_H
 #define SHOT_H
-class Shot
+class Shot: public Renderizable
 {
 public:
-    virtual void demage(HPEntity pEntity)=0;
+    virtual void demage(HPEntity *pEntity)=0;
     virtual bool isUefulShot()=0;
     virtual void getType =0;
     virtual ~Shot();
