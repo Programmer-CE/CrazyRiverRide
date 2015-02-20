4c6b5c03a85c745678c97dd198e2e7cf25476c7f
albinmora <albin.moravalverde@gmail.com>
2015-02-20 00:59:26 -0600

    Modificacion a la clase Shot, debio a falta de codigo

--- src/gui/shot.h
+++ src/gui/shot.h
@@ -6,7 +6,7 @@ class Shot: public Renderizable
 public:
     virtual void demage(HPEntity *pEntity)=0;
     virtual bool isUefulShot()=0;
-    virtual void getType =0;
+    virtual void getType()=0;
     virtual ~Shot();
 };
 #endif // SHOT_H
