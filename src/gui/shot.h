fc3d349381c4e8a49aed6927b7c33f3bd6053511
albinmora <albin.moravalverde@gmail.com>
2015-02-20 00:14:49 -0600

    Creacion del Header para los disparos

---
+++ src/gui/shot.h
@@ -0,0 +1,12 @@
+#include "hpentity.h"
+#ifndef SHOT_H
+#define SHOT_H
+class Shot
+{
+public:
+    virtual void demage(HPEntity pEntity)=0;
+    virtual bool isUefulShot()=0;
+    virtual void getType =0;
+    virtual ~Shot();
+};
+#endif // SHOT_H
