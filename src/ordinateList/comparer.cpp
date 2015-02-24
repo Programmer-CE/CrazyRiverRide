89998b432ceae31c3c8f3e424e3e876c69463f5c
crisferlop <crisrivlop@gmail.com>
2015-02-23 19:22:16 -0600

    Agregagado el paquete de listas ordenadas y cambios pequeños en el diagrama de clases

---
+++ src/ordinateList/comparer.cpp
@@ -0,0 +1,5 @@
+#include "comparer.h"
+
+Comparer::Comparer()
+{
+}
---
+++ src/ordinateList/comparer.h
@@ -0,0 +1,10 @@
+#ifndef COMPARER_H
+#define COMPARER_H
+
+class Comparer
+{
+public:
+    Comparer();
+};
+
+#endif // COMPARER_H
